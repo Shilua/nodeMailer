549c275cf1d100e515b7a5d550a666ebd4a2f47d
maximiliano.vargas <maximiliano.vargas@xappia.com>
2020-10-14 23:24:35 -0300

    first commit

---
+++ enviroment.js
@@ -0,0 +1,10 @@
+{
+    const credentials = {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'cortez.runolfsdottir@ethereal.email',
+            pass: 'XXgv7eS6vC2fjH58UE'
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+var express = require('express');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json()); // body en formato json
+app.use(bodyParser.urlencoded({ extended: false })); //body formulario
+
+app.post('/send-email', (req ,res ) => {
+    const transporter = nodemailer.createTransport(
+        {
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
+            }
+    });
+
+    var mailOptions = {
+        from: 'maximiliano.vargas@xappia.com',
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.text
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+            res.status(500).send(error.message);
+        }
+        else{
+            res.status(200).jsonp(req.body);
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor on");
+});
