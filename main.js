2313ca16bc1a97e6d0c1312549079651e8a6d926
maximiliano.vargas <maximiliano.vargas@xappia.com>
2020-10-15 07:56:04 -0300

    Auth use jsonWebToken

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 var PORT = process.env.PORT  || 3000;
 require('dotenv').config();
-var express = require('express');
-var nodemailer = require('nodemailer');
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 var app = express();
 
 app.use(bodyParser.json()); // body en formato json
 app.use(bodyParser.urlencoded({ extended: false })); //body formulario
 
-app.post('/send-email', (req ,res ) => {
+app.post('/token', (req,res)=>{
+    console.log('user '+ req.user);
+    if (req.body.user == process.env.USERTOKEN && req.body.pass == process.env.USERPASSWORD) {
+        let user = req.user;
+        let token = jwt.sign({user},process.env.SECRETKEY);
+        res.json({token});
+    }
+    else{
+        res.status(400).send('user or pass invalid');
+    }
+});
+
+app.post('/send-email', ensureToken, (req ,res ) => {
+    jwt.verify(req.token, process.env.SECRETKEY, (err, data) =>{
+        if (err) {
+            res.status(403).send('invalid token');
+        } else {
+            sendEmail(req,res);
+        }
+    });
+    
+});
+
+function sendEmail(req, res) {
     const transporter = nodemailer.createTransport(
         {
             service: 'gmail',
@@ -34,7 +57,19 @@ app.post('/send-email', (req ,res ) => {
             res.status(200).jsonp(req.body);
         }
     });
-});
+}
+
+function ensureToken(req, res, next) {
+    let bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        let bearer = bearerHeader.split(' ');
+        let bearerToken = bearer[1];
+        req.token = bearerToken;
+        next()
+    }else{
+        res.send(403).send('missing token')
+    }
+}
 
 app.listen(PORT, () => {
     console.log("Servidor on");
